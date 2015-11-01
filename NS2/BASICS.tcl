8e48e9e5d967ab2e8882f6c26c0102ede50e08ed
kushalvyas <kushal.vyas@djsce.edu.in>
2015-11-01 09:49:03 +0530

    Basic NS2

---
+++ NS2/BASICS.tcl
@@ -0,0 +1,15 @@
+# create a simulator object
+set ns [new Simulator]
+
+# create a file for writing nam trace data
+# filename : out.nam in 'w'=> write mode
+set nf [open out.nam w] 
+
+# put $ns  that is the simulator object ka trace in $nf .. that is
+# put $ns ka all operations in $nf (the file)
+$ns namtrace-all $nf
+
+$ns run
+
+
+# This file is the basic skeleton for ns2 files
